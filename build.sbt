96ac6925d43b46b1418a295054a8b60941b94a80
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-10-06 22:51:02 +0300

    Reimplement the initial cache with Akka typed

--- build.sbt
+++ build.sbt
@@ -5,12 +5,16 @@ ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "com.example"
 ThisBuild / organizationName := "example"
 
+val AkkaVersion = "2.6.16"
+
 lazy val root = (project in file("."))
   .settings(
     name := "scached",
     libraryDependencies ++= Seq(
       scalaTest % Test,
-      "org.scalamock" %% "scalamock" % "5.1.0" % Test
+      "org.scalamock" %% "scalamock" % "5.1.0" % Test,
+      "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+      "com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion % Test
     )
   )
 
--- src/main/scala/io/github/antivanov/scached/Cache.scala
+++ src/main/scala/io/github/antivanov/scached/Cache.scala
@@ -1,28 +1,57 @@
 package io.github.antivanov.scached
 
-import java.util.concurrent.ConcurrentHashMap
+import java.util.UUID
 
+import akka.actor.typed.{ActorRef, ActorSystem, Scheduler}
+import akka.actor.typed.scaladsl.Behaviors
 import io.github.antivanov.scached.Cache.Fetch
 
-import scala.jdk.CollectionConverters._
-import scala.collection.concurrent.Map
 import scala.concurrent.{ExecutionContext, Future}
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.pattern.StatusReply
+import akka.util.Timeout
 
-class Cache[K, V](fetch: Fetch[K, V])(implicit ec: ExecutionContext) {
+import scala.concurrent.duration.DurationInt
+import scala.util.{Failure, Success}
 
-  val map: Map[K, V] = new ConcurrentHashMap[K, V]().asScala
+object CacheActor {
+  sealed trait CacheAction
+  case class GetKeyValue[K, V](key: K, replyTo: ActorRef[StatusReply[V]]) extends CacheAction
+  case class ReplyWithValueAndUpdateCache[K, V](key: K, value: V, replyTo: ActorRef[StatusReply[V]]) extends CacheAction
+  case class ReplyWithError[K, V](key: K, error: Throwable, replyTo: ActorRef[StatusReply[V]]) extends CacheAction
+
+  def apply[K, V](cache: Map[K, V], fetch: Fetch[K, V]): Behaviors.Receive[CacheAction] = Behaviors.receive { (context, message) =>
+    message match {
+      case GetKeyValue(key: K, replyTo: ActorRef[StatusReply[V]]) =>
+        if (cache.contains(key))
+          replyTo ! StatusReply.Success(cache(key))
+        else
+          context.pipeToSelf(fetch(key)) {
+            case Success(value) =>
+              ReplyWithValueAndUpdateCache(key, value, replyTo)
+            case Failure(error) =>
+              ReplyWithError(key, error, replyTo)
+          }
+        Behaviors.same
+      case ReplyWithValueAndUpdateCache(key: K, value: V, replyTo: ActorRef[StatusReply[V]]) =>
+        replyTo ! StatusReply.Success(value)
+        CacheActor(cache.updated(key, value), fetch)
+      case ReplyWithError(_, error: Throwable, replyTo: ActorRef[StatusReply[V]]) =>
+        replyTo ! StatusReply.error(error)
+        Behaviors.same
+    }
+  }
+}
+
+class Cache[K, V](fetch: Fetch[K, V]) {
+
+  import CacheActor._
+  val actorSystem = ActorSystem(CacheActor(Map(), fetch), UUID.randomUUID().toString)
+  implicit val timeout = Timeout(5.seconds)
+  implicit val scheduler: Scheduler = actorSystem.scheduler
 
   def get(key: K): Future[V] =
-    map.get(key) match {
-      case None =>
-        fetch(key).map(value => {
-          //TODO: What if there are concurrent modifications/many threads accessing the value?
-          map.putIfAbsent(key, value)
-          value
-        })
-      case Some(value) =>
-        Future.successful(value)
-    }
+    actorSystem.askWithStatus((ref: ActorRef[StatusReply[V]]) => GetKeyValue(key, ref))
 }
 
 object Cache {
